5de4dde9d9c14b392d4390c6a18b9c037df3a627
Новиков Максим <maxom.nowik@yandex.ru>
2023-05-19 17:45:35 +0400

    Initial project version

---
+++ main.py
@@ -0,0 +1,77 @@
+import socket
+import time
+import views
+
+
+URLS = {
+    '/': views.index,
+    '/blog': views.blog
+}
+
+
+def parse_request(request):
+    method, url = None, None
+    try:
+        parsed = request.split(' ')
+        method, url = parsed[0], parsed[1]
+    except IndexError:
+        time.sleep(3)
+        parsed = request.split(' ')
+        method, url = parsed[0], parsed[1]
+    finally:
+        return (method, url)
+
+
+def generate_headers(method, url):
+    if method != "GET":
+        return ('HTTP/1.1 405 Method not allowed\n\n', 405)
+    if url not in URLS:
+        return ('HTTP/1.1 404 Not found\n\n', 404)
+    return ('HTTP/1.1 200 OK\n\n', 200)
+
+
+def generate_content(code, url):
+    if code == 404:
+        return '<h1>404</h1><p>Not found</p>'
+    if code == 405:
+        return '<h1>405</h1><p>Method not allowed</p>'
+    return URLS[url]()
+
+
+def generate_response(request):
+    method, url = parse_request(request)
+    headers, code = generate_headers(method, url)
+    body = generate_content(code, url)
+    # сокеты принимают только байты, поэтому строку кодируем в байты.
+    return (headers + body).encode()
+
+
+def run():
+    # AF_INET это протокол IP адресса 4 версии
+    # SOCK_STREAM это протокол TCP
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Чтобы отключить защитный механизм, который блокирует недавно
+    # используемый порт примерно на 1,5 минуты, что все данные успели дойти.
+    # передаем следующие аргументы:
+    # сам текущий, название параметра, 1 - как значение True.
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # связать субъект с конкретным адресом и портом
+    server_socket.bind(('localhost', 5000))
+    # чтобы сервер слушал этот порт.
+    server_socket.listen()
+    while True:
+        # получаем сокет и адрес клиента, который обратился на наш сервер
+        client_socket, addr = server_socket.accept()
+        # количество байт в пакете, которые будем получать от клиента
+        request = client_socket.recv(1024)
+        # print(request.decode("utf-8"))
+        print(request)
+        print()
+        print(addr)
+        response = generate_response(request.decode("utf-8"))
+        client_socket.sendall(response)
+        client_socket.close()
+
+
+if __name__ == '__main__':
+    run()
